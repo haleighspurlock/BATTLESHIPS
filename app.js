7a92844d794e4a536bfaef620d691f6048b1a842
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-04-29 15:02:14 -0400

    added grid spaces to user and computer

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const userGrid = document.querySelector('.grid-user')
     const computerGrid = document.querySelector('.grid-computer')
     const displayGrid = document.querySelector('.grid-display')
@@ -9,7 +9,24 @@ document.addEventListener('DOMContentLoded', () => {
     const battleship = document.querySelector('.battleship-container')
     const carrier = document.querySelector('.carrier-container')
     const startButton =  document.querySelector('#start')
-    const rotateButton = doccument.querySelector('#rotate')
+    const rotateButton = document.querySelector('#rotate')
     const turnDisplay = document.querySelector('#whose-turn')
     const infoDisplay = document.querySelector('#info')
+    const userSquares = []
+    const computerSquares = []
+
+    const width = 10
+
+    //create board
+    function createBoard(grid, squares) {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div')
+            square.dataset.id = i
+            grid.appendChild(square)
+            squares.push(square)
+        }
+    }
+
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
 })
