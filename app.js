ee6ab778beab1f9fb6d037a15ebe0c09a4557da5
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-05-02 20:37:14 -0400

    added starting rotation for all ships

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const infoDisplay = document.querySelector('#info')
     const userSquares = []
     const computerSquares = []
+    let isHorizontal = true
 
     const width = 10
 
@@ -92,4 +93,18 @@ document.addEventListener('DOMContentLoaded', () => {
     generate(shipArray[2])
     generate(shipArray[3])
     generate(shipArray[4])
+
+    //rotate the ships
+    function rotate() {
+        if (isHorizontal) {
+            destroyer.classList.toggle('destroyer-container-vertical')
+            submarine.classList.toggle('submarine-container-vertical')
+            cruiser.classList.toggle('cruiser-container-vertical')
+            battleship.classList.toggle('battleship-container-vertical')
+            carrier.classList.toggle('carrier-container-vertical')
+            isHorizontal = false
+        }
+    }
+    rotateButton.addEventListener('click', rotate)
 })
+
