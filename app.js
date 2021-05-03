eeff84560f8c648b30c85c14bc1104072324f5d5
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-05-03 11:57:28 -0400

    cleaned up html error and starting functions

--- app.js
+++ app.js
@@ -116,5 +116,53 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     rotateButton.addEventListener('click', rotate)
+
+    //move around user ship
+    ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+    userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
+    userSquares.forEach(square => square.addEventListener('dragover', dragOver))
+    userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    userSquares.forEach(square => square.addEventListener('drop', dragDrop))
+    userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
+
+    let selectedShipNameWithIndex
+    let draggedShip
+    let draggedShipLength
+
+    ships.forEach(ships => ships.addEventListener('mousedown', (e) => {
+        selectedShipNameWithIndex = e.target.id
+    }))
+
+    function dragStart() {
+        draggedShip = this
+        draggedShipLength = this.childNodes.length
+    }
+
+    function dragOver(e) {
+        e.preventDefault()
+    }
+
+    function dragEnter(e) {
+        e.preventDefault()
+    }
+
+    function dragLeave() {
+        console.log('drag leave')
+    }
+
+    function dragDrop() {
+        let shipNameWithLastId = draggedShip.lastChild.id
+        let shipClass = shipNameWithLastId.slice(0, -2)
+        console.log(shipClass)
+        let lastShipIndex = parseInt(shipNameWithLastId.substr(-1))
+        let shipLastId = lastShipIndex + parseInt(this.dataset.id)
+
+        selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1))
+    }
+
+    function dragEnd() {
+        
+    }
 })
 
