1d0a89d97db3adb30f85524ac7981da7d1027b11
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-05-02 20:22:21 -0400

    all battleships randomly generating

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let current = ship.directions[randomDirection]
         if (randomDirection === 0) direction = 1
         if (randomDirection === 1) direction = 10
-        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction))
+        let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
 
         const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
         const isAtRightEdge = current.some(index => (randomStart + index) % width === width -1)
