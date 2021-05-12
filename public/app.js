c42d0b9293151d93d40eb5fae4cbea1edf5da05a
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-05-12 12:45:33 -0400

    prepping for heroku deploy

--- public/app.js
+++ public/app.js
@@ -322,6 +322,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (currentPlayer === 'user') {
             turnDisplay.innerHTML = 'Your Turn!'
             computerSquares.forEach(square => square.addEventListener('click', function(e) {
+                shotFired = square.dataset.id
                 revealSquare(square.classList)
             }))
         }
